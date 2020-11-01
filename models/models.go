1ac9b3ed8874eb977cc9dde629530d06ed6deaf2
Adam Scholey <hello@adamjs.net>
2020-11-01 12:59:06 +0000

    Simplify Message model

--- models/models.go
+++ models/models.go
@@ -8,29 +8,23 @@ type Identity struct {
 
 // Message defines our message parameters
 type Message struct {
-	Sender    Identity
-	Recipient Identity
-	Advice    string
+	From   string
+	To     string
+	Advice string
 }
 
 // PreviewMessage returns a message to be used for previewing our templates
 func PreviewMessage() *Message {
 	return &Message{
-		Recipient: Identity{
-			Name:  "Jase",
-			Email: "tindall_tindall_give_us_a_wave@example.net",
-		},
-		Sender: Identity{
-			Name:  "Eddie",
-			Email: "eddie_eddie_eddie_howe@example.net",
-		},
+		From:   "Eddie H",
+		To:     "Jase T",
 		Advice: "The more games you win or draw, the more points you'll receive.",
 	}
 }
 
 // Email defines our email parameters
 type Email struct {
-	From    Identity
-	To      Identity
-	Message Message
+	Sender    Identity
+	Recipient Identity
+	Message   Message
 }
