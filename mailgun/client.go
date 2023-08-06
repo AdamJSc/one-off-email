f9ab96d02f9f71342b3927b6584baddb208983b1
Adam Scholey <hello@adamjs.net>
2023-08-06 23:19:49 +0100

    feat: add scaffolding for mailgun client

---
+++ mailgun/client.go
@@ -0,0 +1,18 @@
+package mailgun
+
+import (
+	"context"
+	"one-off-email/domain"
+)
+
+type Client struct{}
+
+func NewClient() (*Client, error) {
+	// TODO: implement new mailgun client
+	return &Client{}, nil
+}
+
+func (c *Client) SendEmail(ctx context.Context, email *domain.Email) (string, error) {
+	// TODO: implement mailgun send email method
+	return "", nil
+}
