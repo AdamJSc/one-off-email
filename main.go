b97816f48c25c9ce1bb81e4208f07fc9ce52067f
Adam Scholey <hello@adamjs.net>
2023-08-06 23:43:23 +0100

    refactor: separate generation of emails from sending

--- main.go
+++ main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"log"
 	"one-off-email/domain"
 	"one-off-email/mailgun"
 	"one-off-email/tpl"
-	"time"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	var (
 		txtTemplatePath  string
 		htmlTemplatePath string
+		send             bool
 	)
 
 	flag.StringVar(&txtTemplatePath, "txt", "", "Specify path to Txt template file (required)")
 	flag.StringVar(&htmlTemplatePath, "html", "", "Specify path to HTML template file (optional)")
+	flag.BoolVar(&send, "send", false, "If true, sends emails via mailgun (otherwise serves web preview of emails)")
 	// TODO: parse config file path
 	flag.Parse()
 
 	if txtTemplatePath == "" {
-		log.Fatalf("-txt flag is required")
+		log.Fatal("-txt flag is required")
 	}
 	// TODO: ensure config file path is not empty
 
 	// TODO: parse config
-	c := &config{}
+	c := &Config{}
 
 	tplClient := tpl.NewClient()
 	mgClient := mustMailgunClient(mailgun.NewClient())
@@ -43,59 +44,14 @@ func main() {
 		emailAgent:       emailAgent,
 	}
 
-	j.do()
-}
+	emails := j.mustParseEmails()
 
-type job struct {
-	txtTemplatePath  string
-	htmlTemplatePath string
-	config           *config
-	msgAgent         *domain.MessageAgent
-	emailAgent       *domain.EmailAgent
-}
-
-func (j *job) do() {
-	for idx, msgConfig := range j.config.messages {
-		res, err := j.processMessageConfig(msgConfig)
-		if err != nil {
-			log.Fatalf("message config index %d: %s", idx, err.Error())
-		}
-		log.Printf("message send sucessful: message config index %d: sent id %q", idx, res)
+	if send {
+		j.mustSendEmails(emails)
+	} else {
+		j.mustServeEmailsPreview(emails)
 	}
 }
-
-func (j *job) processMessageConfig(msgConfig messageConfig) (string, error) {
-	msg, err := j.msgAgent.Generate(domain.GenerateMessageParams{
-		TxtTemplatePath:  j.txtTemplatePath,
-		HTMLTemplatePath: j.htmlTemplatePath,
-		Data:             msgConfig,
-	})
-	if err != nil {
-		return "", fmt.Errorf("cannot generate message: %w", err)
-	}
-
-	// TODO: populate email field values
-	email, err := domain.NewEmail("", "", "", msg)
-	if err != nil {
-		return "", fmt.Errorf("cannot create email: %w", err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	res, err := j.emailAgent.Send(ctx, email)
-	if err != nil {
-		return "", fmt.Errorf("cannot send email: %w", err)
-	}
-
-	return res, nil
-}
-
-type config struct {
-	messages []messageConfig
-}
-
-type messageConfig struct{}
 
 func mustMessageAgent(agent *domain.MessageAgent, err error) *domain.MessageAgent {
 	if err != nil {
@@ -120,3 +76,53 @@ func mustMailgunClient(mg *mailgun.Client, err error) *mailgun.Client {
 
 	return mg
 }
+
+type job struct {
+	txtTemplatePath  string
+	htmlTemplatePath string
+	config           *Config
+	msgAgent         *domain.MessageAgent
+	emailAgent       *domain.EmailAgent
+}
+
+func (j *job) mustParseEmails() []*domain.Email {
+	msgConfigs := j.config.Messages
+	emails := make([]*domain.Email, len(msgConfigs))
+
+	for idx, msgConfig := range msgConfigs {
+		p := domain.GenerateMessageParams{
+			TxtTemplatePath:  j.txtTemplatePath,
+			HTMLTemplatePath: j.htmlTemplatePath,
+			Data:             msgConfig,
+		}
+
+		msg, err := j.msgAgent.Generate(p)
+		if err != nil {
+			log.Fatalf("cannot generate message index %d: %s", idx, err.Error())
+		}
+
+		// TODO: populate email field values once config has been parsed
+		email, err := domain.NewEmail("", "", "", msg)
+		if err != nil {
+			log.Fatalf("cannot create email: message index %d: %s", idx, err.Error())
+		}
+
+		emails[idx] = email
+	}
+
+	return emails
+}
+
+func (j *job) mustServeEmailsPreview(emails []*domain.Email) {
+	// TODO: implement must serve emails preview
+}
+
+func (j *job) mustSendEmails(emails []*domain.Email) {
+	// TODO: implement must send emails
+}
+
+type Config struct {
+	Messages []MessageConfig
+}
+
+type MessageConfig struct{}
